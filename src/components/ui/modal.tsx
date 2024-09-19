63e611b985b8bc35a08762d8f023336a76c38ea1
Bezlkiy <seki4calm@gmail.com>
2024-09-19 23:21:01 +0500

    ✨ feat: вынесение модального окна в отдельную компоненту

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,19 @@
+import { Popover } from "./popover";
+
+type ModalProps = {
+  children: React.ReactNode;
+  onClose: VoidFunction;
+};
+
+const Modal = ({ children, onClose }: ModalProps) => {
+  return (
+    <div
+      className="flex absolute z-10 w-screen h-screen bg-[#11111199] top-0 left-0"
+      onClick={onClose}
+    >
+      <Popover modal>{children}</Popover>;
+    </div>
+  );
+};
+
+export { Modal };
