09410a45be1e1ecc86de3b7d7cd25f9dd5248b4a
Bezliki7 <seki4calm@gmail.com>
2024-12-31 07:55:30 +0500

    ♻️ refactor: значения по умолчанию

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,26 @@ import Providers from "./contexts";
 import { PLAYER_INFO_KEY } from "./pages/settings/settings.constant";
 import { Toaster } from "./components/ui/toaster";
 import Loader from "./components/loader/loader";
+import {
+  DEFAULT_BATTLEFIELD_VALUES,
+  DEFAULT_PLAYER_VALUES,
+} from "./app.constant";
 
 import type { PlayerInfo } from "./pages/settings/settings.interface";
 import type { Battlefield } from "./api/requests/hero-realms/battlefield/battlefield.interface";
 import type { Player } from "./api/requests/hero-realms/player/player.interface";
 
 const App = () => {
-  const [battlefield, setBattlefield] = useState<Battlefield>();
-  const [player, setPlayer] = useState<Player>();
+  const [battlefield, setBattlefield] = useState<Battlefield>(
+    DEFAULT_BATTLEFIELD_VALUES
+  );
+  const [player, setPlayer] = useState<Player>(DEFAULT_PLAYER_VALUES);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetch = async () => {
       const playerInfo = localStorage.getItem(PLAYER_INFO_KEY);
-
+      console.log(playerInfo);
       if (playerInfo) {
         const { battlefieldId, playerId } = JSON.parse(
           playerInfo
@@ -44,14 +50,16 @@ const App = () => {
     return <Loader />;
   }
 
-  if (!(battlefield && player)) {
-    return <Settings />;
-  }
-
   return (
     <Providers battlefield={battlefield} player={player}>
-      <Routes />
-      <Toaster />
+      {!(battlefield && player) ? (
+        <Settings />
+      ) : (
+        <>
+          <Routes />
+          <Toaster />
+        </>
+      )}
     </Providers>
   );
 };
---
+++ src/app.constant.ts
@@ -0,0 +1,18 @@
+export const DEFAULT_BATTLEFIELD_VALUES = {
+  heroes: [],
+  id: 0,
+  name: "",
+  players: [],
+  round: 1,
+};
+
+export const DEFAULT_PLAYER_VALUES = {
+  battlefieldId: 0,
+  currentDamageCount: 0,
+  currentGoldCount: 0,
+  currentTurnPlayer: false,
+  health: 50,
+  id: 1,
+  name: "",
+  heroes: [],
+};
